0a89e9a933afee5a60f32796d29f014639870ef2
Demigod426 <148740823+Demigod426@users.noreply.github.com>
2026-08-20 21:17:14 +0530

    final commit

--- squaresOfAsortedArray.py
+++ squaresOfAsortedArray.py
@@ -1,9 +1,23 @@
+# class Solution:
+#     def sortedSquares(self, nums: List[int]) -> List[int]:
+#         for i in range(len(nums)):
+#             nums[i]=nums[i]**2
+#         for j in range(len(nums)):
+#             for k in range(j+1,len(nums)):
+#                 if nums[j]>nums[k]:
+#                     nums[j],nums[k]=nums[k],nums[j]
+#         return nums
+
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
-        for i in range(len(nums)):
-            nums[i]=nums[i]**2
-        for j in range(len(nums)):
-            for k in range(j+1,len(nums)):
-                if nums[j]>nums[k]:
-                    nums[j],nums[k]=nums[k],nums[j]
-        return nums
+        result=[]
+        l,r=0,len(nums)-1
+        
+        while l<=r:
+            if nums[l]*nums[l]>nums[r]*nums[r]:
+                result.append(nums[l]*nums[l])
+                l+=1
+            else:
+                result.append(nums[r]*nums[r])
+                r-=1
+        return result[::-1]  
