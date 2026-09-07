2b6ce197d68ec1a71052373da18bd0c65dee84d0
Demigod426 <148740823+Demigod426@users.noreply.github.com>
2026-09-07 18:09:29 +0530

    final commit

--- twoIntSumII.py
+++ twoIntSumII.py
@@ -4,4 +4,18 @@ class Solution:
             for j in range(i+1,len(numbers)):
                 if numbers[i] + numbers[j] ==target:
                     return [i+1,j+1]
-        return []
+        return []   # BRUTE FORCE TC:O(n)
+
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        l=0
+        r=len(numbers)-1
+
+        while l<r:
+            total=numbers[l]+numbers[l]
+            if total==target:
+                return [l+1,r+1]
+            elif total<target:
+                l+=1
+            else:
+                r-=1
