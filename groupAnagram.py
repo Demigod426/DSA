c6a3ed756027c433cb68bc62f47bc616c9f6f103
Demigod426 <148740823+Demigod426@users.noreply.github.com>
2026-09-12 21:54:00 +0530

    Update groupAnagram.py

--- groupAnagram.py
+++ groupAnagram.py
@@ -13,6 +13,6 @@ class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         res=defaultdict(list)
         for s in strs:
-            sortedS = ''.join(sorted(s))
+            sortedS=''.join(sorted(s))
             res[sortedS].append(s)
         return list(res.values())
