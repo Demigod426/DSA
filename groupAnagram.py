0537054769b4fff54bab532934f53a305f76c811
Demigod426 <148740823+Demigod426@users.noreply.github.com>
2026-09-12 21:53:10 +0530

    final commit

--- groupAnagram.py
+++ groupAnagram.py
@@ -7,3 +7,12 @@ class Solution:
                 count[ord(c)-ord('a')]+=1
             res[tuple(count)].append(s)
         return list(res.values())
+
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        res=defaultdict(list)
+        for s in strs:
+            sortedS = ''.join(sorted(s))
+            res[sortedS].append(s)
+        return list(res.values())
