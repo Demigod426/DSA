c6039ea2a3359de35be73706fb39a5a3585421d2
Demigod426 <148740823+Demigod426@users.noreply.github.com>
2026-09-17 23:15:21 +0530

    final commit

--- LongestConsecutiveSequence.py
+++ LongestConsecutiveSequence.py
@@ -12,3 +12,16 @@ class Solution:
                     currentlength+=1
                 maxlength=max(maxlength,currentlength)
         return maxlength
+
+#Using HASHMAP
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        mp=defaultdict(int)
+        res=0
+        for num in nums:
+            if not mp[num]:
+                mp[num]=mp[num-1]+mp[num+1]+1
+                mp[num-mp[num-1]]=mp[num]
+                mp[num+mp[num+1]]=mp[num]
+                res=max(res,mp[num])
+        return res
