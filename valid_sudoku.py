e9542c4b36b10a2c071e1ffa29099deddf10f9f2
Demigod426 <148740823+Demigod426@users.noreply.github.com>
2026-09-20 17:39:43 +0530

    2ND SOL

--- valid_sudoku.py
+++ valid_sudoku.py
@@ -1,3 +1,4 @@
+#BRUTE FORCE
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
         for row in range(9):
@@ -29,4 +30,26 @@ class Solution:
                     if board[row][col] in seen:
                         return False
                     seen.add(board[row][col])
+        return True
+
+#HASH MAP
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        cols=defaultdict(set)
+        rows=defaultdict(set)
+        squares=defaultdict(set)
+
+        for r in range(9):
+            for c in range(9):
+                if board[r][c]==".":
+                    continue
+                if ( board[r][c] in rows[r]
+                    or board[r][c] in cols[c]
+                    or board[r][c] in squares[(r//3,c//3)]):
+                    return False
+
+                cols[c].add(board[r][c])
+                rows[r].add(board[r][c])
+                squares[(r//3,c//3)].add(board[r][c])
+
         return True
