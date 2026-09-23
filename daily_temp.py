9c22a89336d2424035127dbcc76ac2f99f554116
Demigod426 <148740823+Demigod426@users.noreply.github.com>
2026-09-23 12:56:35 +0530

    Update daily_temp.py

--- daily_temp.py
+++ daily_temp.py
@@ -1,1 +1,12 @@
 #using stack
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        res=[0]*len(temperatures)
+        stack=[]
+
+        for i,temp in enumerate(temperatures):
+            while stack and temp>temperatures[stack[-1]]:
+                p=stack.pop()
+                res[p]=i-p
+            stack.append(i)
+        return res
