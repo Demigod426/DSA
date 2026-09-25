f2701a27644185c4103f091bc978574427ab6cf7
Demigod426 <148740823+Demigod426@users.noreply.github.com>
2026-09-25 19:59:05 +0530

    refactor

--- car_fleet.py
+++ car_fleet.py
@@ -1,10 +1,11 @@
+#STACK
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
         pair=[[p,s] for p,s in zip(position,speed)]
         stack=[]
 
-        for p,s in sorted(pair)[::-1]:
+        for p,s in sorted(pair)[::-1]:#REVERSAL (RIGHT TO LEFT)
             stack.append((target-p)/s)
-            if len(stack)>=2 and stack[-1]<=stack[-2]:
+            if len(stack)>=2 and stack[-1]<=stack[-2]:#COMPARES THE TOP 2 ELEMENTS 
                 stack.pop()
         return len(stack)
